4dec858b35c4cc5cd03caf8bb39b7d2622eabc56
vagrant <vagrant@precise32.(none)>
2014-02-14 20:35:07 +0000

    database

--- routes/database.js
+++ routes/database.js
@@ -1,47 +1,45 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var db = mongoose.connection;
 
-db.on('error', console.error);
-db.once('open', function() {
-	var userSchema = new Schema({
-	  name:  String,
-	  pwd: String,
-	  type: String,
-	  classesIDArray: [{type: Schema.ObjectId, ref: 'Classes'}]
-	}, {collection: 'userSchema'});
-	module.exports = mongoose.model('Users', userSchema);
-	module.exports = db.usersTable('Users', userSchema);
 
-	var classesSchema = new Schema({
-		name: String,
-		questionIds: [{type: Schema.ObjectId, ref: 'Questions'}]
-	}, {collection: 'classesSchema'});
-	module.exports = mongoose.model('Classes', classesSchema);
-	module.exports = db.classesTable('Classes', classesSchema);
+var userSchema = new Schema({
+  name:  String,
+  pwd: String,
+  type: String,
+  classesIDArray: [{type: Schema.ObjectId, ref: 'Classes'}]
+}, {collection: 'userSchema'});
+mongoose.model('Users', userSchema);
+//module.exports = db.usersTable('Users', userSchema);
 
-	var questionsSchema = new Schema({
-		classId: {type: Schema.ObjectId, ref: 'Classes'},
-		text: String,
-		answerA: String,
-		answerB: String,
-		answerC: String,
-		answerD: String,
-		correctAnswer: String,
-		date: Date,
-		isPublished: Boolean
-	}, {collection: 'questionsSchema'});
-	module.exports = mongoose.model('Questions', questionsSchema);
-	module.exports = db.questionsTable('Questions', questionsSchema);
+var classesSchema = new Schema({
+	name: String,
+	questionIds: [{type: Schema.ObjectId, ref: 'Questions'}]
+}, {collection: 'classesSchema'});
+mongoose.model('Classes', classesSchema);
+//module.exports = db.classesTable('Classes', classesSchema);
 
-	var studentAnswers = new Schema({
-		studentName: String,
-		questionId: String,
-		answer: String
-	}, {collection: 'studentAnswers'});
-	module.exports = mongoose.model('StudentAnswers', studentAnswers);
-	module.exports = db.studentAnswersTable('StudentAnswers', studentAnswers);
-});
+var questionsSchema = new Schema({
+	classId: {type: Schema.ObjectId, ref: 'Classes'},
+	text: String,
+	answerA: String,
+	answerB: String,
+	answerC: String,
+	answerD: String,
+	correctAnswer: String,
+	date: Date,
+	isPublished: Boolean
+}, {collection: 'questionsSchema'});
+mongoose.model('Questions', questionsSchema);
+//module.exports = db.questionsTable('Questions', questionsSchema);
+
+var studentAnswers = new Schema({
+	studentName: String,
+	questionId: String,
+	answer: String
+}, {collection: 'studentAnswers'});
+mongoose.model('StudentAnswers', studentAnswers);
+//module.exports = db.studentAnswersTable('StudentAnswers', studentAnswers);
+
 
 
 mongoose.connect( 'mongodb://localhost/quiz' );
