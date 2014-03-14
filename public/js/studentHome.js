9a8525f4ffcf151d80686fe7c741709c7d1019b0
vagrant <vagrant@precise32.(none)>
2014-03-14 00:10:03 +0000

    changed fieldset text

--- public/js/studentHome.js
+++ public/js/studentHome.js
@@ -11,5 +11,13 @@ $(document).ready(function() {
 function initializePage() {
     $('legend').click(function(){
         $(this).parent().find('.content').slideToggle("slow");
+        var text = $(this).text();
+        if(text.indexOf("Click to ") == -1){
+    		text = "Click to " + text
+    		$(this).text(text);
+        }else{
+    		text = text.replace("Click to ","");
+    		$(this).text(text);
+        }
     });
 }
